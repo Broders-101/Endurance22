2509c22f99a3640b4ba2db00a3237e2510c8c3c3
dezfuture <agrim2000@gmail.com>
2022-01-18 19:20:34 +0530

    LeetCode - medium

--- January-2022/18-Jan/34.find-first-and-last-position-of-element-in-sorted-array.cpp
+++ January-2022/18-Jan/34.find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -44,6 +44,31 @@
  */
 
 // @lc code=start
+
+// Leetcode Medium
+
+// Lower Upper Bound
+
+class Solution
+{
+public:
+    vector<int> searchRange(vector<int> &nums, int target)
+    {
+
+        int lower = lower_bound(nums.begin(), nums.end(), target) - nums.begin();
+        int upper = upper_bound(nums.begin(), nums.end(), target) - nums.begin();
+
+        if (lower == upper)
+        {
+            return {-1, -1};
+        }
+        return {lower, upper - 1};
+    }
+};
+
+// traditional binary search
+
+/*
 class Solution
 {
 public:
@@ -91,4 +116,6 @@ private:
         return res;
     }
 };
+
+*/
 // @lc code=end
