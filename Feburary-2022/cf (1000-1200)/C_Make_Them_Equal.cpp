a20a79d4980b9adba6d91247772dbb49b5172f29
dezfuture <63505820+dezfuture@users.noreply.github.com>
2022-02-12 15:30:24 +0530

    cf

---
+++ Feburary-2022/cf (1000-1200)/C_Make_Them_Equal.cpp
@@ -0,0 +1,149 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fast_io                       \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(0);
+#define endl "\n"
+#define ull unsigned long long
+#define pb push_back
+typedef long long ll;
+typedef pair<int, int> pii;
+const int MOD = 1000000007;
+const int N = 1e3 + 2;
+const int MAXN = 2e5 + 5;
+
+const int M = 1e9 + 7;
+long long mod(long long x)
+{
+    return ((x % M + M) % M);
+}
+long long add(long long a, long long b)
+{
+    return mod(mod(a) + mod(b));
+}
+long long mul(long long a, long long b)
+{
+    return mod(mod(a) * mod(b));
+}
+
+ll modPow(ll a, ll b)
+{
+    if (b == 0)
+        return 1LL;
+    if (b == 1)
+        return a % M;
+    ll res = 1;
+    while (b)
+    {
+        if (b % 2 == 1)
+            res = mul(res, a);
+        a = mul(a, a);
+        b = b / 2;
+    }
+    return res;
+}
+
+void sieve(int n, bool isPrime[])
+{
+    isPrime[0] = isPrime[1] = false;
+    for (int i = 2; i * i <= n; i++)
+    {
+        if (isPrime[i])
+        {
+            for (int j = i * i; j <= n; j += i)
+            {
+                isPrime[j] = false;
+            }
+        }
+    }
+}
+
+int lcm(int a, int b)
+{
+    int c = __gcd(a, b);
+    return (a / c) * b;
+}
+
+int calc(int n)
+{
+    int res = 0;
+    while (n)
+    {
+        res += n % 10;
+        n /= 10;
+    }
+    return res;
+}
+
+void solve()
+{
+
+    int n;
+    char c;
+    cin >> n >> c;
+    string s;
+    cin >> s;
+
+    int res = 0;
+
+    vector<int> ans;
+
+    bool flag = true;
+
+    for (auto x : s)
+    {
+        if (x != c)
+        {
+            flag = false;
+        }
+    }
+
+    if (!flag)
+    {
+        for (int i = 1; i <= n; i++)
+        {
+            flag = true;
+            for (int j = i; j <= n; j++)
+            {
+                flag &= (s[j - 1] == c);
+                j += i - 1;
+            }
+
+            if (flag)
+            {
+                ans.pb(i);
+
+                break;
+            }
+        }
+
+        if (!flag)
+        {
+            ans.pb(n - 1);
+            ans.pb(n);
+        }
+    }
+
+    cout << ans.size() << endl;
+    for (auto x : ans)
+    {
+        cout << x << " ";
+    }
+
+    cout << endl;
+}
+
+int32_t main()
+{
+    fast_io;
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+
+    return 0;
+}
